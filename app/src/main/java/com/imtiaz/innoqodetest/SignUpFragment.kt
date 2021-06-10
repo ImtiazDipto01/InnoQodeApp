8f54046aa5f4acb613e5e2258772a1a37ed8e5c3
imtiaz <programmer.imtiaz@gmail.com>
2021-06-10 11:35:49 +0600

    Add- view binding added

--- app/src/main/java/com/imtiaz/innoqodetest/SignUpFragment.kt
+++ app/src/main/java/com/imtiaz/innoqodetest/SignUpFragment.kt
@@ -5,18 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.imtiaz.innoqodetest.databinding.FragmentSignUpBinding
 
-class SignUpFragment : Fragment() {
+class SignUpFragment : BottomSheetDialogFragment()  {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var binding: FragmentSignUpBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        binding = FragmentSignUpBinding.inflate(inflater)
+        return binding?.root
     }
 }
